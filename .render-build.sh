4dafbe2c0e4a94ea87dd2df007bb01dcef9fc078
MalkovYar <gidroakson@mail.ru>
2025-08-14 16:01:24 +0700

    last

--- .render-build.sh
+++ .render-build.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-# Автоматически создаём структуру, если её нет
+# Создаем структуру, которую ожидает Render
 mkdir -p src/main
-[ -d server ] && mv server src/main/ || true
-[ -d public ] && mv public src/main/ || true
-[ -f package.json ] && mv package.json src/main/ || true
+mv server public package.json src/main/
 
 # Устанавливаем зависимости
 cd src/main && npm install
